5e6622626e26b202e6a98cbd7a0b4bfd44944af2
Richard Fussenegger <Fleshgrinder@users.noreply.github.com>
2020-02-24 15:04:25 +0100

    Freeze objects on native for multi-threading (#6)
    With this change it was also possible to enable additional targets.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
-import org.gradle.internal.os.OperatingSystem
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetPreset
+@file:Suppress("UNUSED_VARIABLE")
+
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinNativeLink
+import org.jetbrains.kotlin.konan.target.HostManager
 
 plugins {
     id("idea")
@@ -20,18 +23,11 @@ allprojects {
     }
 }
 
-kotlin {
-    val ideaActive = System.getProperty("idea.active") == "true"
+val ideaActive = System.getProperty("idea.active") == "true"
 
-    if (ideaActive) {
-        val os = OperatingSystem.current()
-        when {
-            os.isMacOsX -> macosX64("native")
-            os.isLinux -> linuxX64("native")
-            os.isWindows -> mingwX64("native")
-            else -> error("Cannot prepare native target for unknown OS '$os'")
-        }
-    }
+kotlin {
+    // TODO Kotlin has support for many more targets but we do not know how to
+    //   test them. Examples: wasm, linux other than x64, mingw x86
 
     js {
         browser()
@@ -40,15 +36,39 @@ kotlin {
                 debug = false
             }
         }
+        compilations.all {
+            kotlinOptions {
+                moduleKind = "umd"
+                sourceMap = true
+            }
+        }
     }
 
-    jvm()
-
-    val nativeTargets = mutableSetOf<String>()
-    presets.withType<KotlinNativeTargetPreset>().all {
-        nativeTargets.add(name)
-        targetFromPreset(this)
+    jvm {
+        compilations.all {
+            kotlinOptions {
+                System.getenv("KOTLIN_JAVA_HOME")?.let { jdkHome = it }
+            }
+        }
     }
+
+    if (ideaActive) {
+        when {
+            HostManager.hostIsMac -> macosX64("native")
+            HostManager.hostIsLinux -> linuxX64("native")
+            HostManager.hostIsMingw -> mingwX64("native")
+            else -> error("Cannot prepare native target for unknown host: ${HostManager.hostName}")
+        }
+    }
+
+    macosX64()
+    ios()
+    tvos()
+    watchos()
+
+    linuxX64()
+
+    mingwX64()
 
     targets.all {
         compilations.all {
@@ -58,7 +78,6 @@ kotlin {
         }
     }
 
-    @Suppress("UNUSED_VARIABLE")
     sourceSets {
         all {
             languageSettings.progressiveMode = true
@@ -113,15 +132,60 @@ kotlin {
         val nativeTest = maybeCreate("nativeTest")
         nativeTest.dependsOn(nonJvmTest)
 
-        nativeTargets.forEach {
-            targets.getByName(it) {
-                getByName("${it}Main") {
-                    dependsOn(nativeMain)
+        targets.forEach {
+            if (it is KotlinNativeTarget) {
+                getByName("${it.name}Main") {
+                    dependsOn(if (ideaActive) nonJvmMain else nativeMain)
                 }
-                getByName("${it}Test") {
-                    dependsOn(nativeTest)
+                getByName("${it.name}Test") {
+                    dependsOn(if (ideaActive) nonJvmTest else nativeTest)
                 }
             }
+        }
+    }
+}
+
+tasks {
+    val jvmTest by getting(Test::class) {
+        System.getenv("JAVA_TEST_HOME")?.let { executable = "$it/bin/java" }
+    }
+
+    val jvmMatrixTest by registering {
+        doLast {
+            val versionPattern = Regex("JAVA_HOME_[1-9][0-9]*\\.[1-9][0-9]*\\.[1-9][0-9]*(-ea)?_x64")
+            System.getenv().filterKeys { it matches versionPattern }.forEach { (javaVersion, javaHome) ->
+                println(javaVersion)
+                exec {
+                    environment("JAVA_TEST_HOME", javaHome)
+                    commandLine("./gradlew", "jvmTest", "--stacktrace")
+                }
+            }
+        }
+    }
+
+    if (HostManager.hostIsMac) {
+        val linkDebugTestIosX64 by getting(KotlinNativeLink::class)
+        val iosTest by registering(Exec::class) {
+            group = "verification"
+            description = "Run iOS tests with a simulator."
+            dependsOn(linkDebugTestIosX64)
+            commandLine("xcrun", "simctl", "spawn", "--standalone", "iPhone 8", linkDebugTestIosX64.outputFile.get())
+        }
+
+        val linkDebugTestTvosX64 by getting(KotlinNativeLink::class)
+        val tvosTest by registering(Exec::class) {
+            group = "verification"
+            description = "Run tvOS tests with a simulator."
+            dependsOn(linkDebugTestTvosX64)
+            commandLine("xcrun", "simctl", "spawn", "--standalone", "Apple TV", linkDebugTestTvosX64.outputFile.get())
+        }
+
+        val linkDebugTestWatchosX86 by getting(KotlinNativeLink::class)
+        val watchosTest by registering(Exec::class) {
+            group = "verification"
+            description = "Run watchOS tests with a simulator."
+            dependsOn(linkDebugTestWatchosX86)
+            commandLine("xcrun", "simctl", "spawn", "--standalone", "Apple Watch Series 4 - 40mm", linkDebugTestWatchosX86.outputFile.get())
         }
     }
 }
